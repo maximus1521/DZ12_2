f69d9a834c9181ab1adabc037d8455be75f5ba3f
Maximus <maximus1521@gmail.com>
2024-02-19 22:24:59 +0100

    up dated TESTs

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,9 +5,20 @@ from utils import arrs
 class TestArrs(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 3)
+        self.assertEqual(arrs.get([1, 3, 3], 1, "test"), 3)
         self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([4, 6, 6], 1, "test"), 6)
+        self.assertEqual(arrs.get([1, 2, 3, 4], 4, "test"), "test")
+        self.assertEqual(arrs.get([1, 2, 3, 4], 0, "test"), 1)
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1, 2), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 0, 4), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], None, 4), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 5), [2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, None), [2, 3, 4])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 0, 5), [1, 2, 3, 4])
+        self.assertEqual(arrs.my_slice([], 0, 0), [])
